2483c0e57c7e5cd2175de60c5ed206bc10693a85
Petros Petrou <p.petrou@se24.qmul.ac.uk>
2024-11-23 23:26:42 +0000

    Added the downloadMultiple

--- src/Fetch.hs
+++ src/Fetch.hs
@@ -1,17 +1,23 @@
 module Fetch (
-    download
+    download,
+    downloadMultiple
 ) where
 
 import qualified Data.ByteString.Lazy.Char8 as L8
 import Network.HTTP.Simple
 
 type URL = String
+type MultipleURL = [String]
 
 
--- General Download Function to Fetch Data from an Endpoint
+-- | General Download Function to Fetch Data from an Endpoint
 download :: URL -> IO L8.ByteString
 download url = do
     request <- parseRequest url
     response <- httpLBS request
     return $ getResponseBody response
 
+-- | Function to make multiple API Calls
+downloadMultiple :: MultipleURL -> IO [L8.ByteString]
+downloadMultiple urlList = do
+    mapM download urlList

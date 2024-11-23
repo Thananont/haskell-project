2895a177a7054fb0f0749105b439dc77a01cd839
Petros Petrou <p.petrou@se24.qmul.ac.uk>
2024-11-23 23:23:09 +0000

    Type.hs : Data Structures

--- src/Types.hs
+++ src/Types.hs
@@ -1,14 +1,21 @@
 {-# LANGUAGE DeriveGeneric #-}
 
 module Types (
-    Mode (..),
-    Modes (..)
+    Mode(..),
+    Modes,
+    Route(..),
+    RouteSection(..),
+    ServiceType(..),
+    LineStatus(..),
+    Disruption(..),
+    Crowding(..)
 ) where
 
 import GHC.Generics
 
+-- | Data Structures for the Modes
 data Mode = Mode {
-    isType :: String,
+    isType :: String, 
     isTflService :: Bool,
     isFarePaying :: Bool,
     isScheduledService :: Bool,
@@ -16,3 +23,48 @@ data Mode = Mode {
 } deriving (Show, Generic)
 
 type Modes = [Mode]
+
+-- | Data Structures for the Routes
+data Route = Route {
+    routeIsType :: String,
+    routeId :: String,
+    routeName :: String,
+    routeModeName :: String,
+    routeDisruptions :: [Disruption],
+    routeCreated :: String,
+    routeModified :: String,
+    routeLineStatuses :: [LineStatus],
+    routeRouteSections :: [RouteSection]
+} deriving (Show, Generic)
+
+
+data RouteSection = RouteSection {
+    routeSectionIsType :: String,    
+    routeSectionName :: String,
+    direction :: String,
+    originationName :: String,
+    destinationName :: String,
+    originator :: String,
+    destination :: String,
+    serviceType :: String,
+    validTo :: String,
+    validFrom :: String
+} deriving (Show, Generic)
+
+data ServiceType = ServiceType {
+    serviceTypeIsType :: String,   
+    serviceTypeName :: String,
+    serviceTypeUri :: String
+} deriving (Show, Generic)
+
+data LineStatus = LineStatus {
+    lineStatusIsType :: String
+} deriving (Show, Generic)
+
+data Disruption = Disruption {
+    disruptionIsType :: String 
+} deriving (Show, Generic)
+
+data Crowding = Crowding {
+    crowdingIsType :: String
+} deriving (Show, Generic)
